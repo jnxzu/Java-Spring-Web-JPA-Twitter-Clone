c2c22c9461a7606378726fcdad79c12efdd9807b
Janek Bielówka <35338262+jnxzu@users.noreply.github.com>
2020-01-28 10:42:45 +0100

    Update ReactiveUserDao.java

--- src/main/java/twitter/clone/chirper/service/ReactiveUserDao.java
+++ src/main/java/twitter/clone/chirper/service/ReactiveUserDao.java
@@ -12,7 +12,7 @@ public class ReactiveUserDao {
     private ConnectionFactory cf;
 
     public ReactiveUserDao() {
-        this.cf = new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder().host("localhost")
+        this.cf = new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder().host("thechirper.herokuapp.com")
                 .username("postgres").password("admin").database("postgres").build());
     }
 
